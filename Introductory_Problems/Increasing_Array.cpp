8ca7e446c87a753b8fa1b683beb7db95e8fa02b1
thequantummenece <aadeshmalani@gmail.com>
2024-07-08 19:14:24 +0530

    Solved Repetations

---
+++ Introductory_Problems/Increasing_Array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    long long ans = 0;
+    long long max_till;
+    cin>>max_till;
+    for (int i =0;i<n-1;i++){
+        int temp;
+        cin>>temp;
+        if(temp<=max_till){ans = ans + (max_till-temp);}
+        else{max_till = temp;}
+    } 
+    cout<<ans;    
+    return 0;
+}
