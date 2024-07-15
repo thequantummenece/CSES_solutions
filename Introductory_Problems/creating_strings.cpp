40ed5bf3572c2e3c2d8a924139795f695eca9bba
thequantummenece <aadeshmalani@gmail.com>
2024-07-15 07:24:51 +0530

    Solved Creating Strings

--- Introductory_Problems/creating_strings.cpp
+++ Introductory_Problems/creating_strings.cpp
@@ -2,11 +2,25 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <set>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    
+int main() {
+    string n;
+    cin >> n;
+    set<string> s1;
+
+    sort(n.begin(), n.end());
+
+    do {
+        s1.insert(n);
+    } while (next_permutation(n.begin(), n.end()));
+
+    cout << s1.size() << endl;
+
+    for (const auto& itr : s1) {
+        cout << itr << endl;
+    }
+
     return 0;
 }
