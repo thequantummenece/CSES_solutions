c2a17c9e883be42a3f76d71536aaee551045a78b
thequantummenece <aadeshmalani@gmail.com>
2024-07-15 09:37:28 +0530

    Solved Tower of Hamoi

--- Introductory_Problems/Tower_of_hanoi.cpp
+++ Introductory_Problems/Tower_of_hanoi.cpp
@@ -2,11 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <cmath>
 using namespace std;
+
+void TOH(int n, int a,int b,int c){
+    if(n<=1){cout<<a<<" "<<c<<endl;return;}
+    TOH(n-1,a,c,b);
+    cout<<a<<" "<<c<<endl;
+    TOH(n-1,b,a,c);
+}
 
 int main(){
     int n;
     cin>>n;
-    
+    cout<<pow(2,n)-1<<endl;
+    TOH(n,1,2,3);
     return 0;
 }
