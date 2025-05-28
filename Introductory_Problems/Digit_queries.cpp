ba0ff74f7fa47563bcc18005a18df25fed96dd54
thequantummenece <aadeshmalani@gmail.com>
2025-05-28 21:22:33 +0530

    To Work clean

---
+++ Introductory_Problems/Digit_queries.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+    int q;
+    cin>>q;
+    for(int i =0;i<q;i++){
+        long long k;
+        cin>>k;
+        
+    }
+    return 0;
+}
